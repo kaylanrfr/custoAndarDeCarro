34de6269356323205b8871ecf5c4d6865ea6eb79
kaylanrfr <60267658+kaylanrfr@users.noreply.github.com>
2020-10-02 21:51:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
         <label for="fname">Modelo:</label>
         <input type="text" id="modelo" name="modelo" value="">
         <label for="fname">Km por litro:</label>
-        <input type="text" id="kmlitro" name="kmlitro" value="">
+        <input type="number" id="kmlitro" name="kmlitro" value="">
         <label for="fname">Distância:</label>
-        <input type="text" id="kmdis" name="kmdis" value="">
+        <input type="number" id="kmdis" name="kmdis" value="">
         <label for="fname">Preço gasolina:</label>
-        <input type="text" id="gas" name="gas" value="">
+        <input type="number" id="gas" name="gas" value="">
         <input type="submit" value="Submit">
     </form>
     <?php
